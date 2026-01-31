ed0ae00ec028c2ffe3ee6f80a38fce34e853d2fa
Abhinav Jain <jainabhinav639@gmail.com>
2026-02-01 00:42:58 +0530

    testing leaderboard

--- client/src/data/leaderboardData.js
+++ client/src/data/leaderboardData.js
@@ -2,7 +2,7 @@
 // Centralized leaderboard data for teams
 
 const leaderboardData = [
-  { team: 'Hawkins Labs', points: 0, participation: 0, winner: 0, runnerUp: 0, secondRunnerUp: 0 },
+  { team: 'Hawkins Labs', points: 80, participation: 0, winner: 0, runnerUp: 0, secondRunnerUp: 0 },
   { team: 'The Upside Down', points: 0, participation: 0, winner: 0, runnerUp: 0, secondRunnerUp: 0 },
   { team: 'The Demogorgon', points: 0, participation: 0, winner: 0, runnerUp: 0, secondRunnerUp: 0 },
   { team: 'The Signal Seekers', points: 0, participation: 0, winner: 0, runnerUp: 0, secondRunnerUp: 0 },
